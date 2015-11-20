f69d8c0592d518d286f1bf322bf7d46cff0defe0
gaborsoulavy <g.soulavy@gmail.com>
2015-11-20 20:34:52 +0000

    Initial Commit

---
+++ gsnet/gsnet.h
@@ -0,0 +1,19 @@
+//
+//  gsnet.h
+//  gsnet
+//
+//  Created by Gabor Soulavy on 20/11/2015.
+//  Copyright © 2015 Gabor Soulavy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for gsnet.
+FOUNDATION_EXPORT double gsnetVersionNumber;
+
+//! Project version string for gsnet.
+FOUNDATION_EXPORT const unsigned char gsnetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gsnet/PublicHeader.h>
+
+
